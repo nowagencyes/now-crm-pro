2e111f52148d111276e241d39254867a02c22233
nowagencyes <hola@nowagency.es>
2026-05-25 00:04:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://gnaupwesbkmlzdxqxrif.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuYXVwd2VzYmttbGR6eHF4cmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NDcxNjQsImV4cCI6MjA5NTIyMzE2NH0.IKmELkgxZ92HZtemdxHP-WKSKIqbhC3mvrW44B9oa6w";
+const SUPABASE_ANON_KEY = "sb_publishable_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuYXVwd2VzYmttbGR6eHF4cmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NDcxNjQsImV4cCI6MjA5NTIyMzE2NH0.IKmELkgxZ92HZtemdxHP-WKSKIqbhC3mvrW44B9oa6w";
 
 const META_APP_ID = "";
 const TIKTOK_CLIENT_KEY = "";
